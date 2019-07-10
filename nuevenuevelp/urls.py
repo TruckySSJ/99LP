ac4b0e46ca09e76713b298beb9ced09289d55504
TruckySSJ <49452220+TruckySSJ@users.noreply.github.com>
2019-07-10 15:54:41 -0400

    second

--- nuevenuevelp/urls.py
+++ nuevenuevelp/urls.py
@@ -20,5 +20,6 @@ from core import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
+    path('portafolio/', views.portafolio, name="portafolio"),
     path('admin/', admin.site.urls),
 ]

40a5e06b3eebc536894f7511a6ec5e6657af12e3
TruckySSJ <49452220+TruckySSJ@users.noreply.github.com>
2019-07-11 17:10:27 -0400

    third

--- nuevenuevelp/urls.py
+++ nuevenuevelp/urls.py
@@ -17,9 +17,15 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 
+from django.conf import settings
+
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
     path('portafolio/', views.portafolio, name="portafolio"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
